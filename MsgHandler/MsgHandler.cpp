9bd277777f60eb45d82487a5a90b8754709a49ed
tony <yourmathisblowingmymind@gmail.com>
2025-03-09 14:27:38 +0200

    Fix MsgHandler prints

--- MsgHandler/MsgHandler.cpp
+++ MsgHandler/MsgHandler.cpp
@@ -7,9 +7,9 @@
  * 2020-11-18    Done
  *
  *-------------------------------------------------------------------*/
- 
+
 //--- includes ---
- 
+
 #include <MsgHandler.h>
 #include <MC_Helpers.h>
 #include <stdint.h>
@@ -17,7 +17,7 @@
 #define DEBUG_ONRX		0x0001
 #define DEBUG_REGNODE	0x0002
 #define DEBUG_TXMSG		0x0004
-#define DEBUG_ULCK    0x0008
+#define DEBUG_ULCK      0x0008
 
 //--- definitions ---
 
@@ -30,7 +30,7 @@ const uint16_t MsgHandlerMaxLeaseTime = (2 * MaxMsgTime + 2);
 
 /*------------------------------------------------------
  * MsgHandler()
- * constuctor. Register the callback at my instance of
+ * constructor. Register the callback at my instance of
  * the Uart
  * 
  * 2020-05-15 AW Rev A
@@ -44,8 +44,8 @@ MsgHandler::MsgHandler()
 	Cb.callback = (pfunction_pointer_t)MsgHandler::OnMsgRxCb;
 	Cb.op = (void *)this;
 	Uart.Register_OnRxCb(&Cb);
-	//now set default values for no node regsitered
-	for(int16_t i = 0; i < MsgHandler_MaxNodes;i++)
+	//now set default values for no node registered
+	for(int16_t i = 0; i < MsgHandler_MaxNodes; i++)
 	{
 		nodeId[i] = invalidNodeId;
 		TxMsgPending[i] = false;
@@ -71,8 +71,8 @@ void MsgHandler::Open(uint32_t baudrate)
  * Update()
  * needed to call the Update of the underlying Uart as there
  * is no real interrupt driven Rx or Tx here
- * If the Msghandler has been locked for a too long time
- * it will e unlocked here to give the system a chance to recover
+ * If the MsgHandler has been locked for a too long time
+ * it will be unlocked here to give the system a chance to recover
  * 
  * 2020-05-15 AW Rev A
  * 
@@ -87,21 +87,21 @@ void MsgHandler::Update(uint32_t timeNow)
 	{
 		UnLockHandler();
 		#if(DEBUG_MSGHandler & DEBUG_ULCK)
-		Serial.println("Msg: unlocked");
+		std::printf("Msg: unlocked\n");
 		#endif
 	}
 }
 
 /*------------------------------------------------------
  * ResetMsgHandler()
- * to be called, when to upper layers run into a TO
- * does not delete pending messages
- * MsgHandler itself doesn't really have states do be reset
+ * to be called, when the upper layers run into a TO.
+ * Does not delete pending messages.
+ * MsgHandler itself doesn't really have states to be reset,
  * so it's a call to reset the Uart only
  * 
  * 2020-07-25 AW Rev A
  * 
- * ----------------------------------------------------*/
+ * ------------------------------------------------------*/
 void MsgHandler::ResetMsgHandler()
 {
 	Uart.ResetUart();
@@ -116,11 +116,11 @@ void MsgHandler::ResetMsgHandler()
  * 
  * 2020-11-07 AW Rev A
  * 
- * ----------------------------------------------------*/
+ * ------------------------------------------------------*/
 bool MsgHandler::LockHandler()
 {
 	if(isLocked)
-		//is allready locked
+		// is already locked
 		return false;
 	else
 	{
@@ -128,7 +128,6 @@ bool MsgHandler::LockHandler()
 		lockTime = actTime;
 	}	
 	return true;
-
 }
 
 
@@ -136,29 +135,27 @@ bool MsgHandler::LockHandler()
  * UnLockHandler()
  * unlock the handler - to allow for others to access it
  * 
- * 
  * 2020-11-07 AW Rev A
  * 
- * ----------------------------------------------------*/
+ * ------------------------------------------------------*/
 void MsgHandler::UnLockHandler()
 {
 	isLocked = false;
 }
 
-
 /*------------------------------------------------------
  * OnRxHandler(Msg)
- * react to a received Msg
- * first check the CRC. If valid use a switch case
- * to call the registered handler
+ * react to a received Msg.
+ * First check the CRC. If valid, use a switch case
+ * to call the registered handler.
  * 
  * 2020-05-15 AW Rev A
  * 
- * ----------------------------------------------------*/
+ * ------------------------------------------------------*/
  
 void MsgHandler::OnRxHandler(MCMsg *RxMsg)
 {
-uint8_t NodeHandle = FindNode(RxMsg->Hdr.u8NodeNr);
+	uint8_t NodeHandle = FindNode(RxMsg->Hdr.u8NodeNr);
 
 	if((NodeHandle != InvalidSlot) && IsCrcOk((UART_Msg *)RxMsg))
 	{
@@ -171,43 +168,40 @@ uint8_t NodeHandle = FindNode(RxMsg->Hdr.u8NodeNr);
 			case eStatusWord:
 			case eEmergencyMsg:
 				#if(DEBUG_MSGHandler & DEBUG_ONRX)
-				Serial.print("MSG: Rx Sys CMD: ");
-				Serial.println(cmd, HEX);
+				std::printf("MSG: Rx Sys CMD: %X\n", cmd);
 				#endif
 				if(OnRxSysCb[NodeHandle].callback != NULL)
-					OnRxSysCb[NodeHandle].callback(OnRxSysCb[NodeHandle].op,(void *)RxMsg);
+					OnRxSysCb[NodeHandle].callback(OnRxSysCb[NodeHandle].op, (void *)RxMsg);
 				break;
 			case eSdoReadReq:
 			case eSdoWriteReq:
 			case eSdoError:
 				#if(DEBUG_MSGHandler & DEBUG_ONRX)
-				Serial.println("MSG: Rx SDO Response");
+				std::printf("MSG: Rx SDO Response\n");
 				#endif
 				if(OnRxSDOCb[NodeHandle].callback != NULL)
-					OnRxSDOCb[NodeHandle].callback(OnRxSDOCb[NodeHandle].op,(void *)RxMsg);
+					OnRxSDOCb[NodeHandle].callback(OnRxSDOCb[NodeHandle].op, (void *)RxMsg);
 				break;
 			default:
 				break;
 		}
 	}
-	//now after having received a message try to resend any pening Tx message
-	for(int i = 0; i < MsgHandler_MaxNodes;i++)
+	// After having received a message, try to resend any pending Tx message
+	for(int i = 0; i < MsgHandler_MaxNodes; i++)
 	{
 		if(TxMsgPending[i])
 		{
-			//try to send
-			//clear the flag if successfull
-			if(SendMsg(i,&(TxMsg[i])))
+			// try to send; clear the flag if successful
+			if(SendMsg(i, &(TxMsg[i])))
 				TxMsgPending[i] = false;
 			break;
 		}
 	}
-
 }
 
 /*----------------------------------------------------------
- * char FindNode(char)
- * find the NodeHandle in the vector of adresses
+ * uint8_t FindNode(uint8_t)
+ * find the NodeHandle in the vector of addresses
  * 
  * 2020-05-16 AW Header
  * 
@@ -226,13 +220,13 @@ uint8_t MsgHandler::FindNode(uint8_t NodeId)
 }
 
 /*----------------------------------------------------------
- * uint8_t RegisterNode(char)
- * try to register a node with it's node ID.
- * the handerl shall be used as a regference for furhter calls
+ * uint8_t RegisterNode(uint8_t)
+ * try to register a node with its node ID.
+ * The handler shall be used as a reference for further calls.
  * 
  * 2020-05-16 AW Header
  * 
- * --------------------------------------------------------*/
+ * ----------------------------------------------------------*/
 
 uint8_t MsgHandler::RegisterNode(uint8_t thisNodeId)
 {
@@ -248,28 +242,25 @@ uint8_t MsgHandler::RegisterNode(uint8_t thisNodeId)
 		i++;
 	}
 	if(slot != InvalidSlot)
-		nodeId[slot]=(int16_t)thisNodeId;
+		nodeId[slot] = (int16_t)thisNodeId;
 	
 	#if(DEBUG_MSGHandler & DEBUG_REGNODE)
-	Serial.print("Msg: Reg ");
-	Serial.print(thisNodeId, DEC);
-	Serial.print(" at ");
-	Serial.println(slot);
+	std::printf("Msg: Reg %d at %d\n", thisNodeId, slot);
 	#endif
 	
 	return slot;
 }
 
 /*----------------------------------------------------------
- *  int8_t MsgHandler::GetNodeId(uint8_t)
- *  read the nodeId of a regsitered node back
+ * int8_t GetNodeId(uint8_t)
+ * read the nodeId of a registered node back.
  * 
  * 2020-11-01 AW
  * ----------------------------------------------------------*/ 
 
 int8_t MsgHandler::GetNodeId(uint8_t NodeHandle)
 {
-int8_t thisNodeId = -1;
+	int8_t thisNodeId = -1;
 
 	if(NodeHandle < MsgHandler_MaxNodes)
 	{
@@ -278,14 +269,13 @@ int8_t thisNodeId = -1;
 	return thisNodeId;
 }
 		
-
 /*----------------------------------------------------------
- * void UnRegisterNode(char)
- * remove the entry for a given node
+ * void UnRegisterNode(uint8_t)
+ * remove the entry for a given node.
  * 
  * 2020-05-16 AW Header
  * 
- * --------------------------------------------------------*/
+ * ----------------------------------------------------------*/
 
 void MsgHandler::UnRegisterNode(uint8_t NodeHandle)
 {
@@ -296,19 +286,18 @@ void MsgHandler::UnRegisterNode(uint8_t NodeHandle)
 		OnRxSDOCb[NodeHandle].op = NULL;
 		OnRxSysCb[NodeHandle].callback = NULL;
 		OnRxSysCb[NodeHandle].op = NULL;
-		
 	}
 }
 		
 /*----------------------------------------------------------
- * SendMsg(char NodeHandle, MCMsg *TxMsg)
- * if possible send the Msg directly
- * this can be done without copying the Msg into a buffer,
- * as MCUart doest copy
+ * bool SendMsg(uint8_t NodeHandle, MCMsg *NewTxMsg)
+ * if possible, send the Msg directly.
+ * This can be done without copying the Msg into a buffer,
+ * as MCUart does not copy.
  * 
  * 2020-05-10 AW Header
  * 
- * --------------------------------------------------------*/
+ * ----------------------------------------------------------*/
 
 bool MsgHandler::SendMsg(uint8_t NodeHandle, MCMsg *NewTxMsg)
 {
@@ -317,50 +306,47 @@ bool MsgHandler::SendMsg(uint8_t NodeHandle, MCMsg *NewTxMsg)
 	UART_Msg *ThisMsg = (UART_Msg *)NewTxMsg;
 	
 	#if(DEBUG_MSGHandler & DEBUG_TXMSG)
-	Serial.print("Msg: Msg 4 Node ");
-	Serial.print(nodeId[NodeHandle], DEC);
+	std::printf("Msg: Msg 4 Node %d", nodeId[NodeHandle]);
 	#endif
 	
 	if(NodeHandle < MsgHandler_MaxNodes)
 	{
 		returnValue = true;
-		//add Node-Id
+		// add Node-Id
 		ThisMsg->Hdr.u8NodeNr = (uint8_t) nodeId[NodeHandle];
-		//and CRC
-		ThisMsg->u8Data[ThisMsg->Hdr.u8Len] = CalcCRC((const uint8_t *)&(ThisMsg->u8Data[1]), ThisMsg->Hdr.u8Len-1);
+		// and CRC
+		ThisMsg->u8Data[ThisMsg->Hdr.u8Len] = CalcCRC((const uint8_t *)&(ThisMsg->u8Data[1]), ThisMsg->Hdr.u8Len - 1);
 
-		//write or store
+		// write or store
 		if(!(Uart.WriteMsg(ThisMsg)))
 		{
 			// try to store
 			if(TxMsgPending[NodeHandle])
 			{
-				//there is already a stored message for this one
+				// there is already a stored message for this one
 				returnValue = false;
-
 				#if(DEBUG_MSGHandler & DEBUG_TXMSG)
-				Serial.println("Msg:  full!");
+				std::printf("Msg:  full!\n");
 				#endif
 			}
 			else
 			{
-				//store this message
-				//will result in a return of true so the calling
-				//instance will consider the TX to be successfull
+				// store this message
+				// will result in a return of true so the calling instance will consider the TX to be successful
 				TxMsgPending[NodeHandle] = true;
-				for(uint8_t i=0;i<=ThisMsg->Hdr.u8Len;i++)
+				for(uint8_t i = 0; i <= ThisMsg->Hdr.u8Len; i++)
 					TxMsg[NodeHandle].Raw.u8Data[i] = ThisMsg->u8Data[i];	
 				
 				#if(DEBUG_MSGHandler & DEBUG_TXMSG)
-				Serial.println("Msg: stored");
+				std::printf("Msg: stored\n");
 				#endif
 			}
 		}
 		else
 		{
-			//return of the Uart was true - so successfull TX
+			// return of the Uart was true - so successful TX
 			#if(DEBUG_MSGHandler & DEBUG_TXMSG)
-			Serial.println("Msg: sent");
+			std::printf("Msg: sent\n");
 			#else
 			;
 			#endif
@@ -370,13 +356,13 @@ bool MsgHandler::SendMsg(uint8_t NodeHandle, MCMsg *NewTxMsg)
 }
 
 /*----------------------------------------------------------
- * Register_onRxCb(function_holder *cb)
+ * Register_OnRxSDOCb(uint8_t, pfunction_holder *Cb)
  * store the function and object pointer for the callback
  * called in case of a successful Rx
  * 
  * 2020-05-10 AW Header
  * 
- * --------------------------------------------------------*/
+ * ----------------------------------------------------------*/
 
 void MsgHandler::Register_OnRxSDOCb(uint8_t NodeHandle, pfunction_holder *Cb)
 {
@@ -388,13 +374,13 @@ void MsgHandler::Register_OnRxSDOCb(uint8_t NodeHandle, pfunction_holder *Cb)
 }
 
 /*----------------------------------------------------------
- * Register_onRxCb(function_holder *cb)
+ * Register_OnRxSysCb(uint8_t, pfunction_holder *Cb)
  * store the function and object pointer for the callback
  * called in case of a successful Rx
  * 
  * 2020-05-10 AW Header
  * 
- * --------------------------------------------------------*/
+ * ----------------------------------------------------------*/
 
 void MsgHandler::Register_OnRxSysCb(uint8_t NodeHandle, pfunction_holder *Cb)
 {
@@ -406,39 +392,40 @@ void MsgHandler::Register_OnRxSysCb(uint8_t NodeHandle, pfunction_holder *Cb)
 }
 
 /*----------------------------------------------------------
- * bool IsCrcOk(UART_Msg *)
+ * bool IsCrcOk(const UART_Msg *)
  * check the CRC of a Msg against the CRC which can be calculated
- * based on the message
+ * based on the message.
  * 
  * 2020-05-10 AW Header
  * 
- * --------------------------------------------------------*/
+ * ----------------------------------------------------------*/
 
 bool MsgHandler::IsCrcOk(const UART_Msg *Msg)
 {
-	return (Msg->u8Data[Msg->Hdr.u8Len] == CalcCRC(&(Msg->u8Data[1]), Msg->Hdr.u8Len-1)); 	
+	return (Msg->u8Data[Msg->Hdr.u8Len] == CalcCRC(&(Msg->u8Data[1]), Msg->Hdr.u8Len - 1)); 	
 }
+
 /*----------------------------------------------------------
- * char CalcCRC(char *buffer,int len)
- * calculate the CRC of a given buffer
+ * uint8_t CalcCRC(const uint8_t *buffer, int len)
+ * calculate the CRC of a given buffer.
  * 
  * 2020-05-10 AW Header
  * 
- * --------------------------------------------------------*/
+ * ----------------------------------------------------------*/
 
-uint8_t MsgHandler::CalcCRC(const uint8_t *buffer,int len)
+uint8_t MsgHandler::CalcCRC(const uint8_t *buffer, int len)
 {
-  uint8_t calcCRC = 0xFF;
-  for(uint8_t i = 0;i<len;i++)
-  {
-    calcCRC = calcCRC ^((uint8_t *)buffer)[i];
-    for(uint8_t j = 0;j < 8;j++)
-    {
-      if(calcCRC & 0x01)
-        calcCRC = (calcCRC >> 1) ^ 0xd5;
-      else
-        calcCRC = (calcCRC >> 1);
-    }
-  }
-return calcCRC;
+	uint8_t calcCRC = 0xFF;
+	for(uint8_t i = 0; i < len; i++)
+	{
+		calcCRC = calcCRC ^ ((uint8_t *)buffer)[i];
+		for(uint8_t j = 0; j < 8; j++)
+		{
+			if(calcCRC & 0x01)
+				calcCRC = (calcCRC >> 1) ^ 0xd5;
+			else
+				calcCRC = (calcCRC >> 1);
+		}
+	}
+	return calcCRC;
 }
